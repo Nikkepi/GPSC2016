55fe543085fb70f291713c8bd04534b466bf7939
Vuorio Leo <olevu002@edu.mamk.fi>
2016-07-19 12:52:38 +0300

    bukaak

---
+++ Gravoyager/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
+  
+
+    public float RotateSpeed = 80f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(-Vector3.forward * RotateSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.forward * RotateSpeed * Time.deltaTime);
+    }
+
+  
+
+}
+
+
+   
+   
+      
