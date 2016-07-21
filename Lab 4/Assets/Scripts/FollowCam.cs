eb4bb4fa2e00b24dc2f161a445416a58a159046d
Vuorio Leo <olevu002@edu.mamk.fi>
2016-07-21 11:39:59 +0300

    Vitusti tutoriaaleja

---
+++ Lab 4/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour
+{
+    static public FollowCam S;
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    // The point of interest
+    public GameObject poi;
+    // The desired Z pos of the camera
+    public float camZ;
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+    void FixedUpdate()
+    {
+        Vector3 destination;
+        // don't do anything if there is no poi
+        if (poi == null)
+        {
+            destination = Vector3.zero;
+
+        }else
+        {
+            destination = poi.transform.position;
+            //If poi is a projectile, chack to see if it's at rest
+            if(poi.tag == "Projectile")
+            {
+
+                if(poi.GetComponent<Rigidbody>().IsSleeping())
+                {
+
+                    poi = null;
+                    return;
+                }
+            }
+            
+        }
+            
+        // Get the position of the poi
+
+        
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Retain a destination.z of CamZ
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+    }
+
+
+}
---
+++ Lab 4/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+    static public Slingshot S; 
+    public float velocityMult = 6f;
+    public GameObject prefabProjectile;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    public GameObject launchPoint;
+    void Awake()
+    {
+        Transform launchPointTrans =
+        transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+        S = this;
+    }
+
+    void OnMouseEnter()
+    {
+        
+        launchPoint.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+    void OnMouseDown()
+    {
+        // The player has pressed the mouse utton while over Slinshot
+        aimingMode = true;
+        // Instantiate a projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Start it at the launch point
+        projectile.transform.position = launchPos;
+        // Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void Update()
+    {
+        // If the Slingshot is not in aiming mode, don't run this script
+        if (!aimingMode)
+            return;
+        // Get the current mouse position in 2D screen coordinates
+        Vector3 mousePos2D = Input.mousePosition;
+        // Convert the mouse position to 3D world coordinates
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        // Find the delta from the launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        // Limit mouseDelta to the radius of the Slingshot SphereCollider
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        // Move the projectile to this new position
+        
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        {
+            // The mouse has been released
+            aimingMode = false;
+            projectile.GetComponent<Rigidbody>().isKinematic = false;
+            projectile.GetComponent<Rigidbody>().velocity =
+             -mouseDelta * velocityMult;
+            FollowCam.S.poi = projectile;
+            projectile = null;
+        }
+    }
+
+
+}
+
+
